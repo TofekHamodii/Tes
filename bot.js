dbbc320123aa51bc20bbe63f6590056e6616e241
TofekHamodii <43014320+TofekHamodii@users.noreply.github.com>
2018-10-22 16:55:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,55 +1,77 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '&'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Ritual 500 Soon`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+
+client.on('ready', function() {
+    console.log(`i am ready ${client.user.username}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+const developers = ["452379478618800129","",""]
+const adminprefix = "*";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Now Playig   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leaveserver")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Now Watching   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Now Listening   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'stream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**Now Streaming   ${argresult}**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
 });
 
 client.on('message', message => {
-   
+    
     let args = message.content.split(' ').slice(1).join(' ');
-   
+    
   if (message.content === 'ping') {
-      message.channel.send(`<@${message.author.id}> Ping..!`)
+      message.channel.send(`<@-{message.author.id}> Ping..!`)
   }
- 
- 
-  if (message.content.startsWith('&bc')) {
+  
+  
+  if (message.content.startsWith('*bc')) {
           if (!args[0]) {
-message.channel.send("**&bc <message>**");
+message.channel.send("***bc <message>**");
 return;
 }
 message.guild.members.forEach(m => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
    m.send(`${args}`);
- 
+
 });
   }
  
 });
-
 
 client.login(process.env.BOT_TOKEN);
